78f2aefd9997f12e94b8e0f3dfe76361ad1b8c26
root <root@Rasoul.>
2025-11-07 15:58:22 +0330

    chore(settings): add simplejwt and set restframework config

--- config/settings.py
+++ config/settings.py
@@ -37,7 +37,19 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # rest_framework
+    'rest_framework',
+    'rest_framework_simplejwt',
 ]
+
+# rest_framework config
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
