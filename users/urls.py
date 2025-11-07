8b5395c18b70cc62bd8e01e801f03b5c7e4c8c1f
root <root@Rasoul.>
2025-11-07 16:24:43 +0330

    feat(users): add access and refresh token

--- users/urls.py
+++ users/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from . import views
 
 urlpatterns = [
     path('register/', views.UserRegisterCreateAPIView.as_view(), name='user-register'),
     path('me/', views.UserInfoDetailView.as_view(), name='user-info'),
+    path('token/', TokenObtainPairView.as_view(), name='access-token'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token-refresh'),
+
+
 ]
